1a1da15c0676d197516cf62cc65d327f9a8c8328
zing <x1zing@126.com>
2019-01-30 16:23:09 +0800

    init

---
+++ src/main/java/com/zing/es/sample/EsSampleApplication.java
@@ -0,0 +1,17 @@
+package com.zing.es.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author ?
+ */
+@SpringBootApplication
+public class EsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSampleApplication.class, args);
+    }
+
+}
+
